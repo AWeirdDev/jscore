d34a0d1afbfbb99dd76acc93480db25a76afc4d2
AWeirdDev <aweirdscratcher@gmail.com>
2026-03-08 15:49:53 +0800

    nice touches

--- wrapper.h
+++ wrapper.h
@@ -1,10 +1,14 @@
+// generated with artificial intelligence
+// sounds bad. but whatever the fuck
+// so i just add some personal touches in case you hate its format
+
 #if defined(__APPLE__)
-    // Apple platforms always have JSC as a system framework
+    // apple platforms always have JSC as a system framework
     #include <JavaScriptCore/JavaScript.h>
 #elif defined(JSC_INCLUDE_PATH)
-    // Explicit path provided via -DJSC_INCLUDE_PATH="..." clang arg
+    //  path provided from -DJSC_INCLUDE_PATH="..."
     #include JSC_INCLUDE_PATH
 #else
-    // Fallback: downloaded/built headers next to this wrapper
+    // downloaded/built headers next to the wrapper
     #include "JavaScriptCore/JavaScript.h"
 #endif
