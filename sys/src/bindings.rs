b1cbdf8ad0f00b97bb4addfb8ca61b24bb37675c
AWeirdDev <aweirdscratcher@gmail.com>
2026-03-13 16:22:30 +0800

    only on macs

--- sys/src/bindings.rs
+++ sys/src/bindings.rs
@@ -1,6 +1,6 @@
 #![allow(nonstandard_style)]
 
-#[cfg(any(target_os = "macos", target_os = "linux"))]
+#[cfg(any(target_os = "macos"))]
 pub type PidT = std::os::raw::c_int;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
